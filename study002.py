658930d6e40c409d8126b78d5a3ce580664e53b6
ubuntu <1156110902@qq.com>
2017-03-08 16:31:25 +0800

    add study002.py

---
+++ study002.py
@@ -0,0 +1,21 @@
+class Student (object):
+	def __init__(self,name,score):
+		self.name=name
+		self.score=score
+
+	def print_score(self):
+		print ('%s %s ' % (self.name,self.score))
+
+	def get_name(self):
+		return self.__name
+
+	def get_score(self):
+		return self.__score
+
+	def set_score(self,score):
+		if 0<=score<=100:
+			self.__score=score
+		else:
+			raise ValueError('bad score')
+
+
