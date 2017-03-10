e912d151fa2c9f9f8290a8395491eb0771ab1870
ubuntu <1156110902@qq.com>
2017-03-10 12:08:05 +0800

    add study005.py

---
+++ study005.py
@@ -0,0 +1,12 @@
+class  Student(object):
+	def __init__(self,name):
+		self.name=name
+	def __call__(self):
+		print ('My name is %s' % self.name)	
+s=Student('lijun')
+print s()
+print(callable(Student))
+print (callable(max))
+print (callable([1,2,3]))
+print (callable('str'))
+print callable(None)
