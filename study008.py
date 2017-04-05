a55e8c32ec67ef69bb698d86a024a25d0c335ffc
ubuntu <1156110902@qq.com>
2017-04-05 20:49:37 +0800

    Don't understand it

--- study008.py
+++ study008.py
@@ -1,15 +1,15 @@
 class Dict(dict):
 	'''
-	Simple dict but support access as x.y style
+	Simple dict but support access as x.y style.
 
-	>>>d1=Dict()
+	>>>d1 = Dict()
 	>>>d1['x']=100
 	>>>d1.x
 	100
 	>>>d1.y=200
 	>>>d1['y']
 	200
-	>>>d2=Dict[a=1,b=2,c='3']
+	>>>d2=Dict(a=1,b=2,c='3')
 	>>>d2.c
 	'3'
 	>>>d2['empty']
