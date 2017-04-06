4332fea00d5ad44f22768879d9700c90d5ee32fe
ubuntu <1156110902@qq.com>
2017-04-06 10:31:53 +0800

    picking & unpicking

--- study009.py
+++ study009.py
@@ -18,3 +18,10 @@ print(json.dumps(s,default=student2dict))
 print('in other world:\n')
 print(json.dumps(s,default=lambda obj:obj.__dict__))
 
+
+def dict2student(d):
+	return Student(d['name'],d['age'],d['score'])
+
+json_str='{"age":20,"score":88,"name":"Bob"}'
+print(json.loads(json_str,object_hook=dict2student))
+
