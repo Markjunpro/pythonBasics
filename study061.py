7b4714479e60ba619ebf7aed47ba497bcab8db24
Markjun <markjunpro@gmail.com>
2018-11-24 23:52:39 +0800

    mysql.connect

---
+++ study061.py
@@ -0,0 +1,18 @@
+import mysql.connector
+conn = mysql.connector.connect(user='root',password='username',database='test')
+cursor = conn.cursor()
+cursor.execute('creat table user (id varchar(20) primary key ,name varchar(20))')
+cursor.execute('insert into user (id,name) values (%s ,%s)',['1','username'])
+print(cursor.rowcount)
+print('\n')
+conn.commit()
+cursor.close()
+
+cursor = conn.cursor()
+cursor.excute('select * from user where id = %s',('1',))
+values = cursor.fetchall()
+print(values)
+print('\n')
+print(cursor.close())
+conn.close()
+
