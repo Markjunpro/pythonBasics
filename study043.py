a9e63463701b3323f7605bdb332e0af9613a0c43
Markjun <markjunpro@gmail.com>
2018-11-24 23:30:17 +0800

    delete sentitive files

--- study043.py
+++
@@ -1,8 +0,0 @@
-import socket
-
-s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-for data in [b'lijun',b'lihui',b'mother',b'family']:
-    s.sendto(data,('127.0.0.1',9999))
-    print(s.recv(1024).decode('utf-8'))
-s.close()
-
